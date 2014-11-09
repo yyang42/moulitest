7d6a8fd11479fef5c3f4e6ccc51d6c0f63f3b5d9
yyang <yan.yaang@gmail.com>
2014-11-09 17:41:32 +0100

    add back isblank

--- main.c
+++ main.c
@@ -843,6 +843,18 @@ UT_TEST(ft_strtrimc)
 
 #ifdef EXTRA_JUSCHAEF
 
+UT_TEST(ft_isblank)
+{
+	UT_ASSERT_EQ(ft_isblank('\n'), isblank('\n'));
+	UT_ASSERT_EQ(ft_isblank(' '), isblank(' '));
+	UT_ASSERT_EQ(ft_isblank('\t'), isblank('\t'));
+	UT_ASSERT_EQ(ft_isblank('\v'), isblank('\v'));
+	UT_ASSERT_EQ(ft_isblank('8'), isblank('8'));
+	UT_ASSERT_EQ(ft_isblank('p'), isblank('p'));
+	UT_ASSERT_EQ(ft_isblank('/'), isblank('/'));
+	UT_ASSERT_EQ(ft_isblank('&'), isblank('&'));
+}
+
 UT_TEST(ft_putnendl)
 {
 	int		out;

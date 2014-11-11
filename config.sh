4bc0d065b6f9d024de725a911773a7f8f5a0c3e9
yyang <yan.yaang@gmail.com>
2014-11-11 17:31:04 +0100

    fix typo 2

--- config.sh
+++ config.sh
@@ -1,5 +1,3 @@
-export FRAMEWORK_PATH=../_testframework
-
 export LIBFT_PATH=~/Copy/activities/libft
 export LIBFT_EXTRA_TESTS="ft_memdup,ft_isblank,ft_strrev,ft_strtrimc,\
 			ft_strcapitalize,ft_islower,ft_isupper,ft_ispunct,ft_abs,\

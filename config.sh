84b2c712e7562c24c8f4703103cbb0242a00c32a
yyang <yan.yaang@gmail.com>
2014-11-11 18:00:00 +0100

    update

--- config.sh
+++
@@ -1,8 +0,0 @@
-# Make a copy of this file and name it "config"
-# DO NOT DELETE "config.template"
-
-export LIBFT_PATH="~/Annee01/Libft_project/rendu"
-export LIBFT_EXTRA_TESTS="ft_JUSCHAEF,ft_isspace,ft_replace_char,ft_isblank,ft_swap,\
-	ft_memswap,ft_islower,ft_isupper,ft_ispunct,ft_abs,ft_factorial,\
-	ft_strcapitalize,ft_rotone,ft_nrot,ft_printable_rotone,ft_strupcase,\
-	ft_strlowcas"

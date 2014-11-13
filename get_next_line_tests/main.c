19a332ec218b8486ec85e0a8cce68a5a54535992
yyang <yan.yaang@gmail.com>
2014-11-13 12:41:41 +0100

    add framework for get_next_line

---
+++ get_next_line_tests/main.c
@@ -0,0 +1,19 @@
+#include <testframework.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <malloc/malloc.h>
+#include <ctype.h>
+#include <math.h>
+#include <get_next_line.h>
+
+<TEST_DEF_BLOCK>
+
+int	main(void)
+{
+<MAIN_TEST_BLOCK>
+
+	UT_RUN_ALL_TESTS();
+	return (0);
+}
---
+++ get_next_line_tests/tests/get_next_line.spec.c
@@ -0,0 +1,2 @@
+char *line = "abc";
+UT_ASSERT_EQ(get_next_line(12, &line), abs(12));

4f4e6db8eb080be52daae5dd538f763ae93c8794
yyang <yan.yaang@gmail.com>
2014-11-13 19:57:29 +0100

    first get next line test working!!!!!!!!!

--- get_next_line_tests/tests/get_next_line.spec.c
+++ get_next_line_tests/tests/get_next_line.spec.c
@@ -1,2 +1,16 @@
-char *line = "abc";
-UT_ASSERT_EQ(get_next_line(12, &line), abs(12));
+char *line;
+int		out;
+int		p[2];
+
+line = strdup("===");
+out = dup(1);
+pipe(p);
+dup2(p[1], 1);
+write(1, "aaa\nbbb\nccc\n", 12);
+dup2(out, 1);
+get_next_line(p[0], &line);
+UT_ASSERT_EQ(strcmp(line, "aaa"), 0);
+get_next_line(p[0], &line);
+UT_ASSERT_EQ(strcmp(line, "bbb"), 0);
+get_next_line(p[0], &line);
+UT_ASSERT_EQ(strcmp(line, "ccc"), 0);

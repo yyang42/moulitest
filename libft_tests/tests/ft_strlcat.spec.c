a5865a74d7942c0012c8cc0c064f42fed5b0c864
yyang <yan.yaang@gmail.com>
2014-11-15 20:14:12 +0100

    update strlcat

--- libft_tests/tests/ft_strlcat.spec.c
+++ libft_tests/tests/ft_strlcat.spec.c
@@ -1,5 +1,4 @@
 char	buf[10];
-
 bzero(buf, 10);
 strcpy(buf, "abc");
 ft_strlcat(buf, "abcdefghijklmnop", 10);
@@ -11,3 +10,8 @@ UT_ASSERT_EQ(ft_strlcat(buf, "def", 10), 6);
 bzero(buf, 10);
 memset(buf, 'a', 10);
 UT_ASSERT_EQ(ft_strlcat(buf, "ccc", 10), 13);
+
+UT_ASSERT_EQ(ft_strlcat(strdup("abc\0\0\0"), "ccc", 0), strlcat(strdup("abc\0\0\0"), "ccc", 0));
+UT_ASSERT_EQ(ft_strlcat(strdup("abc\0\0\0"), "ccc", 1), strlcat(strdup("abc\0\0\0"), "ccc", 1));
+UT_ASSERT_EQ(ft_strlcat(strdup("abc\0\0\0"), "ccc", 6), strlcat(strdup("abc\0\0\0"), "ccc", 6));
+UT_ASSERT_EQ(ft_strlcat(strdup("abc\0\0\0"), "ccc", 10), strlcat(strdup("abc\0\0\0"), "ccc", 10));

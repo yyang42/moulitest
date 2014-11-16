fafc51b2c251af7e968adc4ae6ccc5f9f08f973d
yyang <yan.yaang@gmail.com>
2014-11-16 22:48:51 +0100

    update test framework, add correction check_all

--- libft_tests/testsuite_qperez/main.c
+++ libft_tests/testsuite_qperez/main.c
@@ -1617,7 +1617,6 @@ int				uf_test_memchr(void)
 			D_ERROR
 		if (memchr(itab, i, sizeof(itab)) != ft_memchr(itab, i, sizeof(itab)))
 			D_ERROR
-		printf("%s\n", ltab);
 		if (memchr(ltab, i, sizeof(ltab)) != ft_memchr(ltab, i, sizeof(ltab)))
 			D_ERROR
 		++i;

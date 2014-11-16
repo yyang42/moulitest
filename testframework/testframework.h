d4cccb8400aef395b7b0a4766c409c450ba3e751
yyang <yan.yaang@gmail.com>
2014-11-16 16:29:06 +0100

    update framework

--- testframework/testframework.h
+++ testframework/testframework.h
@@ -7,12 +7,20 @@
 # define UT_TEST(name)			void ut_test_ ## name(int *param)
 # define UT_ASSERT(cond)		if(!(cond)) { ut_last_err = #cond; *param = 1; return;}
 # define UT_SEGV(test_)			puts("[\033[34;1mSEGV\033[0m]");
-# define UT_RUN_TEST(t_,i_)		printf("%s%-30s", "[\033[33mUT\033[0m] ", t_->name); t_->test(i_); if(*(i_)) \
-								{ printf("[\033[31;1mFAIL\033[0m]\t\033[31;1m( %s )\033[0m\n", ut_last_err); _test_fails++; } \
-								else { puts("[\033[32;1mOk \033[0m\033[33;1m*\033[0m]"); }
+# define UT_RUN_TEST(t_,i_)		printf("%s%-30s", "[\033[33mUT\033[0m] ", t_->name); \
+								t_->test(i_); \
+								if(*(i_) == 2) { \
+									printf("[\033[33;1mWARN\033[0m]\t\033[33;1m( %s )\033[0m\n", ut_last_err); \
+								} \
+								else if(*(i_) == 1) { \
+									printf("[\033[31;1mFAIL\033[0m]\t\033[31;1m( %s )\033[0m\n", ut_last_err); _test_fails++; \
+								} \
+								else { \
+									puts("[\033[32;1mOk !\033[0m]"); \
+								}
 # define UT_ADD_TEST(name)		ut_add_test_(&ut_test_ ## name, #name)
 # define UT_RUN_ALL_TESTS()		ut_run_all_tests_()
-# define UT_ASSERT_WARN(cond)	if(!(cond)) { ut_last_err = #cond; *param = 1; printf("%s\n", "blabla"); return;};
+# define UT_ASSERT_OPTIONAL(cond)	if(!(cond)) { ut_last_err = #cond; *param = 2; }
 
 /*
  *  Bonus
@@ -28,10 +36,12 @@ typedef struct				ut_test_list_s
 	ut_test					test;
 	char					*name;
 	struct ut_test_list_s	*next;
+
 }							ut_test_list_t;
 
 extern ut_test_list_t		*ut_tests;
 extern char					*ut_last_err;
+extern char					*ut_optional;
 
 void						ut_sigsegv_(int);
 ut_test_list_t				*ut_create_list_(ut_test, char *);

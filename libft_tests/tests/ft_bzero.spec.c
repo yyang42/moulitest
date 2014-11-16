4a28b7de6d7721edbe295763012d784abd71527f
yyang <yan.yaang@gmail.com>
2014-11-16 16:54:54 +0100

    fix bzero

--- libft_tests/tests/ft_bzero.spec.c
+++ libft_tests/tests/ft_bzero.spec.c
@@ -9,13 +9,3 @@ UT_ASSERT_EQ(memcmp(b1, b2, 10), 0);
 b1[0] = 1;
 ft_bzero(b1, 0);
 UT_ASSERT_EQ(b1[0], 1);
-
-
-// UT_ASSERT(1 == 1);
-// UT_ASSERT(0 == 1);
-int a = 1;
-int b = 2;
-UT_ASSERT((a == b) || (a == b));
-// UT_ASSERT_OP(0 == 1);
-// UT_ASSERT(1 == 1);
-// UT_ASSERT(1 == 0);

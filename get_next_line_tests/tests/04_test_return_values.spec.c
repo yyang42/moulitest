2eb12fc303005dceb000239cb77106b44a866e33
yyang <yan.yaang@gmail.com>
2014-11-18 18:58:28 +0100

    update tests/04_test_return_values.spec.c

--- get_next_line_tests/tests/04_test_return_values.spec.c
+++ get_next_line_tests/tests/04_test_return_values.spec.c
@@ -1,5 +1,5 @@
-/* Thank you bwyyoung for pointing out the problem.
- */
+/* Thank you bwyyoung for pointing out the problem. */
+
 char 	*line;
 int		out;
 int		p[2];
@@ -11,16 +11,28 @@ pipe(p);
 
 fd = 1;
 dup2(p[1], fd);
-write(fd, "aaaaaaaaaaaaaa", 14);
+write(fd, "abc\n\n", 5);
 close(p[1]);
 dup2(out, fd);
 
+/* Read abc and new line */
 gnl_ret = get_next_line(p[0], &line);
-UT_ASSERT_EQ(gnl_ret, 0);
+UT_ASSERT_EQ(gnl_ret, 1);
 UT_ASSERT(line != NULL);
-UT_ASSERT_EQ(strcmp(line, "aaaaaaaaaaaaaa"), 0);
+UT_ASSERT_EQ(strcmp(line, "abc"), 0);
 
-/* get_next_line should return NULL when there is nothing to read. */
+/* Read new line */
 gnl_ret = get_next_line(p[0], &line);
-UT_ASSERT(line == NULL || *line == '\0');
+UT_ASSERT_EQ(gnl_ret, 1);
+UT_ASSERT(line != NULL);
+UT_ASSERT_EQ(strcmp(line, ""), 0);
+
+/* Read again, but meet EOF */
+gnl_ret = get_next_line(p[0], &line);
 UT_ASSERT_EQ(gnl_ret, 0);
+UT_ASSERT(line == NULL);
+
+/* Let's do it once again */
+gnl_ret = get_next_line(p[0], &line);
+UT_ASSERT_EQ(gnl_ret, 0);
+UT_ASSERT(line == NULL);

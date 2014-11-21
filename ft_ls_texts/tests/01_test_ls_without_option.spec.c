c098555245dc6dd1cb3f959278f3043c4af833f6
yyang <yan.yaang@gmail.com>
2014-11-21 13:48:36 +0100

    first working test

--- ft_ls_texts/tests/01_test_ls_without_option.spec.c
+++ ft_ls_texts/tests/01_test_ls_without_option.spec.c
@@ -1,8 +1,14 @@
 reset_sandbox();
 sandbox_cmd("touch aaa bbb ccc");
+UT_ASSERT(strcmp(ft_ls("."), "aaa\nbbb\nccc\n") == 0);
 UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);
 
 reset_sandbox();
+UT_ASSERT(strcmp(ft_ls("."), "") == 0);
+UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);
+
+reset_sandbox();
+sandbox_cmd("touch file{001..1000}");
 UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);
 
 // printf("\n=====  ls  ========\n");
@@ -10,18 +16,3 @@ UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);
 // printf("===== ft ls =======\n");
 // printf("%s", ft_ls(". | cat -e"));
 // printf("==================\n");
-
-// ft_ls(".");
-
-// reset_sandbox(); sandbox_cmd("touch aaa");
-
-// reset_sandbox();
-// ls("-1");
-// // sandbox_cmd("touch aaa bbb ccc");
-// ft_ls(".");
-// UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);
-// reset_sandbox(); sandbox_cmd("touch aaa bbb");
-// UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);
-
-// reset_sandbox(); sandbox_cmd("touch aaa ccc");
-// UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);

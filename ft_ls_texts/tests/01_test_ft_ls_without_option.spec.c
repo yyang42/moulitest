8cf9dcae13cad6a99b0f6463dec93ecff008d97a
yyang <yan.yaang@gmail.com>
2014-11-21 14:02:23 +0100

    update tests

--- ft_ls_texts/tests/01_test_ft_ls_without_option.spec.c
+++ ft_ls_texts/tests/01_test_ft_ls_without_option.spec.c
@@ -11,6 +11,12 @@ reset_sandbox();
 sandbox_cmd("touch file{001..1000}");
 UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);
 
+reset_sandbox();
+sandbox_cmd("touch aaa bbb");
+UT_ASSERT(strcmp(ls("-1"), ft_ls("")) == 0);
+
+reset_sandbox();
+
 // printf("\n=====  ls  ========\n");
 // printf("%s", ls("-1 | cat -e"));
 // printf("===== ft ls =======\n");

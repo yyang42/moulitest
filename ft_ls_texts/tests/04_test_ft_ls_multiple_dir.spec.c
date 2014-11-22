3ffb2f8b44490748831410b9e5e70275079ff453
yyang <yan.yaang@gmail.com>
2014-11-22 14:31:01 +0100

    tests/04_test_ft_ls_multiple_dir.spec.c

--- ft_ls_texts/tests/04_test_ft_ls_multiple_dir.spec.c
+++ ft_ls_texts/tests/04_test_ft_ls_multiple_dir.spec.c
@@ -1,9 +1,19 @@
 reset_sandbox();
 sandbox_cmd("mkdir mydir mydir2 && touch mydir/{aaa,bbb,ccc} && touch mydir2/{111,222,333}");
-printf("\n=====  ls  ========\n");
-printf("%s", ls("-1 mydir mydir2 | cat -e"));
-printf("===== ft ls =======\n");
-printf("%s", ft_ls("mydir mydir2 | cat -e"));
-printf("==================\n");
+UT_ASSERT(strcmp(ls("-1 mydir mydir2"), ft_ls("mydir mydir2")) == 0);
 
-// UT_ASSERT(strcmp(ls("-1"), ft_ls(".")) == 0);
+reset_sandbox();
+sandbox_cmd("mkdir mydir mydir2 && touch mydir/file{10..40} && touch mydir2/file{30..200}");
+UT_ASSERT(strcmp(ls("-1 mydir mydir2"), ft_ls("mydir mydir2")) == 0);
+
+reset_sandbox();
+sandbox_cmd("mkdir mydir{1..7} && touch mydir{1..7}/file{1..99}");
+UT_ASSERT(strcmp(ls("-1 mydir{1..7}"), ft_ls("mydir{1..7}")) == 0);
+
+/*
+printf("\n=====  ls  ========\n");
+printf("%s", ls("-1 mydir{1..7} | cat -e"));
+printf("===== ft ls =======\n");
+printf("%s", ft_ls("mydir{1..7} | cat -e"));
+printf("==================\n");
+*/

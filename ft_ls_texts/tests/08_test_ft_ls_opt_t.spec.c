f42b75fbba5dc61c82d8c6437338a1312f016e61
yyang <yan.yaang@gmail.com>
2014-11-22 17:24:24 +0100

    update tests/08_test_ft_ls_opt_t.spec.c

--- ft_ls_texts/tests/08_test_ft_ls_opt_t.spec.c
+++ ft_ls_texts/tests/08_test_ft_ls_opt_t.spec.c
@@ -5,21 +5,18 @@ sandbox_cmd("touch a b c d e && touch 20420101 c");
 UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 reset_sandbox();
-sandbox_cmd("touch 20140103 a && touch 20140101 b && touch 20140102 c");
+sandbox_cmd("touch 02010103 a && touch 01010101 b && touch 03010102 c");
 UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
-// reset_sandbox();
-// sandbox_cmd("mkdir -p a b c AA BB CC");
-// UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+reset_sandbox();
+sandbox_cmd("touch fileA{1..20} fileB{1..20} fileC{1..20} && touch 02010101 fileA{1..20} && touch 01010707 fileB{1..20} && touch 03011212 fileC{1..20}");
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
-// reset_sandbox();
-// sandbox_cmd("mkdir -p POP mok POPO liu && touch PIP mik PIPI lui");
-// UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
-
-
+/*
 printf("\n=====  ls  ========\n");
 printf("%s", ls(cmd));
 printf("===== ft ls =======\n");
 printf("%s", ft_ls(cmd));
 printf("==================\n");
 
+*/

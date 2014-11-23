7b8b97e923cd5acfc1e21b68245d0e6d2db6da3c
yyang <yan.yaang@gmail.com>
2014-11-23 10:43:25 +0100

    update

--- libft_tests/tests/ft_memmove.spec.c
+++ libft_tests/tests/ft_memmove.spec.c
@@ -5,8 +5,8 @@ ft_memmove(testA1 + 1, testA1, 5);
 ft_memmove(testA1 + 1, testA1, 0);
 memmove(testA2 + 1, testA2, 5);
 memmove(testA2 + 1, testA2, 0);
-UT_ASSERT_EQ(strcmp(testA1, "aabcde"), 0);
-UT_ASSERT_EQ(strcmp(testA1, testA2), 0);
+UT_ASSERT(strcmp(testA1, "aabcde") == 0);
+UT_ASSERT(strcmp(testA1, testA2) == 0);
 
 char	testB1[] = "abcdef";
 char	testB2[] = "abcdef";
@@ -15,8 +15,8 @@ ft_memmove(testB1, testB1 + 1, 5);
 ft_memmove(testB1, testB1 + 1, 0);
 memmove(testB2, testB2 + 1, 5);
 memmove(testB2, testB2 + 1, 0);
-UT_ASSERT_EQ(strcmp(testB1, "bcdeff"), 0);
-UT_ASSERT_EQ(strcmp(testB1, testB2), 0);
+UT_ASSERT(strcmp(testB1, "bcdeff") == 0);
+UT_ASSERT(strcmp(testB1, testB2) == 0);
 
 /* test edge cases */
 char	test[] = "aaa";

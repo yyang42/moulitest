323073feec3ddbac4f5df2ae19d7b4ae1d9f7de6
yyang <yan.yaang@gmail.com>
2014-11-23 09:20:50 +0100

    [gnl] improve error handling tests

--- get_next_line_tests/tests/05_test_error_handling.spec.c
+++ get_next_line_tests/tests/05_test_error_handling.spec.c
@@ -1,2 +1,8 @@
+UT_ASSERT_EQ(get_next_line(-99, NULL), -1);
+UT_ASSERT_EQ(get_next_line(-1, NULL), -1);
 UT_ASSERT_EQ(get_next_line(1, NULL), -1);
 UT_ASSERT_EQ(get_next_line(99, NULL), -1);
+
+char 	*line;
+UT_ASSERT_EQ(get_next_line(-99, &line), -1);
+UT_ASSERT_EQ(get_next_line(-1, &line), -1);

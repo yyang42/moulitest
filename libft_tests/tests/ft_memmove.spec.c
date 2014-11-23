1f976f64441cc252bf9f4109018c0826bb172acd
yyang <yan.yaang@gmail.com>
2014-11-23 10:06:45 +0100

    fix memmove

--- libft_tests/tests/ft_memmove.spec.c
+++ libft_tests/tests/ft_memmove.spec.c
@@ -5,12 +5,16 @@ ft_memmove(test + 1, test, 0);
 UT_ASSERT_EQ(strcmp(test, "aabcde"), 0);
 
 /* test edge cases */
-UT_ASSERT(ft_memmove(NULL, test, 0) == NULL);
-UT_ASSERT(ft_memmove(test, NULL, 0) == NULL);
-UT_ASSERT(ft_memmove(NULL, NULL, 0) == NULL);
-UT_ASSERT(ft_memmove(NULL, test, 100) == NULL);
-UT_ASSERT(ft_memmove(test, NULL, 100) == NULL);
-UT_ASSERT(ft_memmove(NULL, NULL, 100) == NULL);
-UT_ASSERT(ft_memmove(NULL, test, -100) == NULL);
-UT_ASSERT(ft_memmove(test, NULL, -100) == NULL);
-UT_ASSERT(ft_memmove(NULL, NULL, -100) == NULL);
+UT_ASSERT(ft_memmove(NULL, test, 0) == memmove(NULL, test, 0));
+UT_ASSERT(strcmp(memmove(test, NULL, 0), ft_memmove(test, NULL, 0)) == 0);
+UT_ASSERT(ft_memmove(NULL, NULL, 0) == memmove(NULL, NULL, 0));
+UT_ASSERT(ft_memmove(NULL, NULL, 100) == memmove(NULL, NULL, 100));
+UT_ASSERT(ft_memmove(NULL, NULL, -100) == memmove(NULL, NULL, -100));
+
+/* The original memmove segfault or stops on those cases;
+
+	memmove(NULL, test, 100);
+	memmove(test, NULL, 100);
+	memmove(NULL, test, -100);
+	memmove(test, NULL, -100);
+*/

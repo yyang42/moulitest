7449e1f6d05af33c038568d9225a7709e383a88f
yyang <yan.yaang@gmail.com>
2014-11-23 09:24:08 +0100

    [gnl] Fix issue #23: Test when get_next_line returns 0 #23

--- get_next_line_tests/tests/11_test_few_lines_of_16.spec.c
+++ get_next_line_tests/tests/11_test_few_lines_of_16.spec.c
@@ -2,6 +2,7 @@ char 	*line;
 int		out;
 int		p[2];
 int		fd;
+int		ret;
 
 out = dup(1);
 pipe(p);
@@ -31,5 +32,5 @@ get_next_line(p[0], &line);
 UT_ASSERT_EQ(strcmp(line, "cdefghijklmnopqr"), 0);
 get_next_line(p[0], &line);
 UT_ASSERT_EQ(strcmp(line, "stuvwxzabcdefghi"), 0);
-get_next_line(p[0], &line);
-UT_ASSERT_EQ(strcmp(line, ""), 0);
+ret = get_next_line(p[0], &line);
+UT_ASSERT(ret == 0);
--- get_next_line_tests/tests/12_test_line_of_4.spec.c
+++ get_next_line_tests/tests/12_test_line_of_4.spec.c
@@ -2,6 +2,7 @@ char 	*line;
 int		out;
 int		p[2];
 int		fd;
+int		ret;
 
 out = dup(1);
 pipe(p);
@@ -13,5 +14,5 @@ close(p[1]);
 dup2(out, fd);
 get_next_line(p[0], &line);
 UT_ASSERT_EQ(strcmp(line, "abcd"), 0);
-get_next_line(p[0], &line);
-UT_ASSERT_EQ(strcmp(line, ""), 0);
+ret = get_next_line(p[0], &line);
+UT_ASSERT(ret == 0);

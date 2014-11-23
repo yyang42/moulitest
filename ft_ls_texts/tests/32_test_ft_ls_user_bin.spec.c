31348d394fbf460e00d3135dbb790aca7ac3958e
yyang <yan.yaang@gmail.com>
2014-11-23 22:30:13 +0100

    fix sort by time

--- ft_ls_texts/tests/32_test_ft_ls_user_bin.spec.c
+++ ft_ls_texts/tests/32_test_ft_ls_user_bin.spec.c
@@ -10,16 +10,11 @@ UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 cmd = "-l /usr";
 UT_ASSERT(strcmp(ls("-l /usr > ls_out"), ft_ls("-l /usr > ft_ls_out")) == 0);
 
-
-cmd = "-t /usr";
-printf("\n=====  ls  ========\n");
-printf("%s", ls(cmd));
-printf("===== ft ls =======\n");
-printf("%s", ft_ls(cmd));
-printf("==================\n");
+cmd = "-tl /usr";
 UT_ASSERT(strcmp(ls("-t /usr"), ft_ls("-t /usr")) == 0);
-// UT_ASSERT(strcmp(ls("-alt /usr/bin > ls_out"), ft_ls("-alt /usr/bin > ft_ls_out")) == 0);
 
+cmd = "-tl /usr";
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
-// cmd = "-alRt /usr/bin";
-// UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+cmd = "-tla /usr";
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
--- ft_ls_texts/tests/40_test_ft_ls_opt_mix.spec.c
+++ ft_ls_texts/tests/40_test_ft_ls_opt_mix.spec.c
@@ -1,19 +1,19 @@
 char *cmd;
 
 cmd = "";
-// reset_sandbox();
-// sandbox_cmd("mkdir dir{1..5} .hdir{1..5}");
-// sandbox_cmd("touch dir1/file{1..5} dir1/.hfile{1..5}");
-// sandbox_cmd("touch .hdir1/file{1..5} .hdir1/.hfile{1..5}");
-// sandbox_cmd("touch .hdir1/file{1..5} .hdir1/.hfile{1..5}");
-// sandbox_cmd("touch a && ln -s a symlink");
+reset_sandbox();
+sandbox_cmd("mkdir dir{1..5} .hdir{1..5}");
+sandbox_cmd("touch dir1/file{1..5} dir1/.hfile{1..5}");
+sandbox_cmd("touch .hdir1/file{1..5} .hdir1/.hfile{1..5}");
+sandbox_cmd("touch .hdir1/file{1..5} .hdir1/.hfile{1..5}");
+sandbox_cmd("touch a && ln -s a symlink");
 
-// cmd = "-lR .";
+cmd = "-lR .";
 
-// // printf("\n===================\n");
-// // printf("%s", ls(cmd));
-// // printf("\n===================\n");
-// // printf("%s", ft_ls(cmd));
-// // printf("\n===================\n");
+// printf("\n===================\n");
+// printf("%s", ls(cmd));
+// printf("\n===================\n");
+// printf("%s", ft_ls(cmd));
+// printf("\n===================\n");
 
-// UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);

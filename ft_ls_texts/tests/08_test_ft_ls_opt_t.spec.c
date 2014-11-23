cd1c125efb754be95e822876cff0fdc3dc8b78a0
yyang <yan.yaang@gmail.com>
2014-11-23 10:45:17 +0100

    merge .gitignore

--- ft_ls_texts/tests/08_test_ft_ls_opt_t.spec.c
+++ ft_ls_texts/tests/08_test_ft_ls_opt_t.spec.c
@@ -5,7 +5,7 @@ sandbox_cmd("touch a b c d e && touch 20420101 c");
 UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 reset_sandbox();
-sandbox_cmd("touch 20140103 a && touch 20140101 b && touch 2014012 c");
+sandbox_cmd("touch 20140103 a && touch 20140101 b && touch 20140102 c");
 UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 // reset_sandbox();

315421a6d3f0a8d2986819b6a70743446f94586b
yyang <yan.yaang@gmail.com>
2014-11-26 23:41:09 +0100

    update ft_ls

--- ft_ls_texts/tests/02_test_ft_ls_opt_R.spec.c
+++ ft_ls_texts/tests/02_test_ft_ls_opt_R.spec.c
@@ -14,20 +14,41 @@ UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 reset_sandbox();
 sandbox_cmd("mkdir .dirA && mkdir .dirA/dirB && touch .dirA/dirB/file");
+// printf("\n===================\n");
+// printf("%s", ls(cmd));
+// printf("\n===================\n");
+// printf("%s", ft_ls(cmd));
+// printf("\n===================\n");
 UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 reset_sandbox();
 sandbox_cmd("mkdir .dir1A .dir1B && mkdir .dir1A/dir2A && touch .dir1A/dir2A/file3");
+// printf("\n===================\n");
+// printf("%s", ls(cmd));
+// printf("\n===================\n");
+// printf("%s", ft_ls(cmd));
+// printf("\n===================\n");
 UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 reset_sandbox();
 sandbox_cmd("mkdir .dir1A .dir1B && mkdir .dir1A/dir2{A,B} && touch .dir1A/dir2A/file3A");
+// printf("\n===================\n");
+// printf("%s", ls(cmd));
+// printf("\n===================\n");
+// printf("%s", ft_ls(cmd));
+// printf("\n===================\n");
 UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
-/*
-printf("\n===================\n");
-printf("%s", ls(cmd));
-printf("\n===================\n");
-printf("%s", ft_ls(cmd));
-printf("\n===================\n");
-*/
+
+cmd = "-1lR";
+
+reset_sandbox();
+sandbox_cmd("mkdir .dirA && mkdir .dirA/dirB && touch .dirA/dirB/file");
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+
+// printf("\n===================\n");
+// printf("%s", ls(cmd));
+// printf("\n===================\n");
+// printf("%s", ft_ls(cmd));
+// printf("\n===================\n");
+

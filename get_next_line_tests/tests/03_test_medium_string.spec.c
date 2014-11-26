ef426ff11de7dec977e35e037a4a4fee8b17a68c
yyang <yan.yaang@gmail.com>
2014-11-26 11:33:56 +0100

    update tests

--- get_next_line_tests/tests/03_test_medium_string.spec.c
+++ get_next_line_tests/tests/03_test_medium_string.spec.c
@@ -2,12 +2,12 @@ char 	*line;
 int		out;
 int		p[2];
 char 	*str;
-int		len = 50;
 
 str = (char *)malloc(1000 * 1000);
 *str = '\0';
-while (len--)
-	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
 out = dup(1);
 pipe(p);
 dup2(p[1], 1);
---
+++ get_next_line_tests/tests/40_hard_test_medium_string.spec.c
@@ -0,0 +1,19 @@
+char 	*line;
+int		out;
+int		p[2];
+char 	*str;
+int		len = 50;
+
+str = (char *)malloc(1000 * 1000);
+*str = '\0';
+while (len--)
+	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+out = dup(1);
+pipe(p);
+dup2(p[1], 1);
+
+write(1, str, strlen(str));
+close(p[1]);
+dup2(out, 1);
+get_next_line(p[0], &line);
+UT_ASSERT(strequ(line, str));

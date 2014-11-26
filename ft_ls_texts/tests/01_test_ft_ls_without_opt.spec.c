87518415ecdb913bd0aea7073816ecba01e35fc7
yyang <yan.yaang@gmail.com>
2014-11-26 23:09:09 +0100

    update ls tests

--- ft_ls_texts/tests/01_test_ft_ls_without_opt.spec.c
+++ ft_ls_texts/tests/01_test_ft_ls_without_opt.spec.c
@@ -22,8 +22,6 @@ reset_sandbox();
 sandbox_cmd("touch aaa");
 UT_ASSERT(strcmp(ls("-1 aaa"), ft_ls("-1 aaa")) == 0);
 
-reset_sandbox();
-
 // printf("\n=====  ls  ========\n");
 // printf("%s", ls("-1 | cat -e"));
 // printf("===== ft ls =======\n");
--- ft_ls_texts/tests/33_test_ft_ls_symlink.spec.c
+++ ft_ls_texts/tests/33_test_ft_ls_symlink.spec.c
@@ -3,7 +3,7 @@ sandbox_cmd("mkdir mydir && ln -s mydir symdir && touch mydir/file{1..5}");
 UT_ASSERT(strcmp(ls("-1l symdir"), ft_ls("-1l symdir")) == 0);
 
 /* This test never match because the ls and ft_ls are launched differently
-	May as different user (tty vs current_group)
+	Both commands may be launched as different user (tty vs current_group)
 
 	UT_ASSERT(strcmp(ls("-1l /dev/fd"), ft_ls("-1l /dev/fd")) == 0);
 */

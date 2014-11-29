0c207f7f29d93c50defb07e12eb1dfdc337d565a
yyang <yan.yaang@gmail.com>
2014-11-29 16:26:28 +0100

    update ls symlink

--- ft_ls_texts/tests/23_test_ft_ls_symlink.spec.c
+++ ft_ls_texts/tests/23_test_ft_ls_symlink.spec.c
@@ -1,16 +1,14 @@
 reset_sandbox();
 sandbox_cmd("mkdir mydir && ln -s mydir symdir && touch mydir/file{1..5}");
 
-// printf("\n===================\n");
-// printf("%s", ls("-1l symdir"));
-// printf("\n===================\n");
-// printf("%s", ft_ls("-1l symdir"));
-// printf("\n===================\n");
-
 UT_ASSERT(strcmp(ls("-1l symdir"), ft_ls("-1l symdir")) == 0);
 
-/* This test never match because the ls and ft_ls are launched differently
-	Both commands may be launched as different user (tty vs current_group)
+UT_ASSERT(strcmp(ls("-1 symdir"), ft_ls("-1 symdir")) == 0);
 
-	UT_ASSERT(strcmp(ls("-1l /dev/fd"), ft_ls("-1l /dev/fd")) == 0);
+/*
+printf("\n===================\n");
+printf("%s", ls("-1 symdir"));
+printf("\n===================\n");
+printf("%s", ft_ls("-1 symdir"));
+printf("\n===================\n");
 */

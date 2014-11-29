9c899bebf3008bee1996fe1ad1c20c2798576e11
Cedric LEGRAND <celegran@e3r9p7.42.fr>
2014-11-29 14:08:19 +0100

    Move bonus test to the end. Make tests affected by system bug as warnings

--- ft_ls_texts/tests/25_test_ft_ls_hide_no_hide.spec.c
+++ ft_ls_texts/tests/25_test_ft_ls_hide_no_hide.spec.c
@@ -5,4 +5,4 @@ cmd = "-1lR";
 reset_sandbox();
 
 sandbox_cmd("mkdir .dirA && mkdir .dirA/dirB && touch .dirA/dirB/file");
-UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+UT_ASSERT_W(strcmp(ls(cmd), ft_ls(cmd)) == 0);

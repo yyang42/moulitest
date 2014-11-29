405a9281a7b5630a3b408026a6d7d0458fc12ea4
yyang <yan.yaang@gmail.com>
2014-11-29 21:02:35 +0100

    update error handling for ft_ls

--- ft_ls_texts/tests/31_error_handling.spec.c
+++ ft_ls_texts/tests/31_error_handling.spec.c
@@ -14,10 +14,29 @@ cmd = "-Z_this_option_does_not_exist";
 UT_ASSERT(strlen(ft_ls_err(cmd)) > 0);
 
 cmd = "-la--t";
+
+// printf("\n===================\n");
+// printf("{%s}", ls_err(cmd));
+// printf("\n===================\n");
+// printf("{%s}", ft_ls_err(cmd));
+// printf("\n===================\n");
+UT_ASSERT(strlen(ft_ls_err(cmd)) > 0);
+
+
+cmd = "\"\"";
 // printf("\n===================\n");
 // printf("{%s}", ls_err(cmd));
 // printf("\n===================\n");
 // printf("{%s}", ft_ls_err(cmd));
 // printf("\n===================\n");
 
-UT_ASSERT(strlen(ft_ls_err(cmd)) > 0);
+UT_ASSERT(strstr(ft_ls_err(cmd), "open") != NULL);
+
+cmd = "''";
+// printf("\n===================\n");
+// printf("{%s}", ls_err(cmd));
+// printf("\n===================\n");
+// printf("{%s}", ft_ls_err(cmd));
+// printf("\n===================\n");
+
+UT_ASSERT(strstr(ft_ls_err(cmd), "open") != NULL);

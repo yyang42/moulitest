5f577a48a7885104237c0b36c5b47cff731717c5
Cedric LEGRAND <celegran@student.42.fr>
2014-11-29 18:24:27 +0100

    Added ls test for opt U

---
+++ ft_ls_texts/tests/84_bonus_test_ft_ls_opt_U.spec.c
@@ -0,0 +1,14 @@
+reset_sandbox();
+char *cmd;
+
+cmd = "-lU /usr/bin";
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+
+cmd = "-lUr /usr/bin";
+UT_ASSERT(strcmp(ls("-l /usr > ls_out"), ft_ls("-l /usr > ft_ls_out")) == 0);
+
+cmd = "-tu1 /usr/bin";
+UT_ASSERT(strcmp(ls("-t /usr"), ft_ls("-t /usr")) == 0);
+
+cmd = "-turl /usr/bin";
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);

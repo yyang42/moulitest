9b372684a8572a27c5b2de7e1555baf7fe7a7a23
yyang <yan.yaang@gmail.com>
2014-11-30 19:35:20 +0100

    add T option

---
+++ ft_ls_texts/tests/bonus_09_test_opt_f.spec.c
@@ -0,0 +1,18 @@
+char *cmd;
+
+reset_sandbox();
+sandbox_cmd("touch a B");
+
+cmd = "-f";
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+
+cmd = "-fl";
+UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+
+/*
+printf("\n=====  ls  ========\n");
+printf("%s", ls(cmd));
+printf("===== ft ls =======\n");
+printf("%s", ft_ls(cmd));
+printf("==================\n");
+*/

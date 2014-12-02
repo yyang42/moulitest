3262538a9f8eaaa19256f07d4ca8c1d10d4693af
yyang <yan.yaang@gmail.com>
2014-12-02 12:58:21 +0100

    update ft_ls

--- ft_ls_tests/tests/bonus_34_test_wtf.spec.c
+++ ft_ls_tests/tests/bonus_34_test_wtf.spec.c
@@ -3,5 +3,5 @@ char *cmd = "-l";
 cmd = "-1R /usr/share/norminette/rbx/2.1/gems/redis-3.1.0";
 UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
-UT_ASSERT_W("Pas encore de test pour /" == 0);
+UT_ASSERT_W(!"Test needed for './ft_ls -R /'");
 

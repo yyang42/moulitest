092eaa4d3c0d89b2d9859b202348146e72029d6d
yyang <yan.yaang@gmail.com>
2014-12-09 12:21:08 +0100

    update ft_ls l

--- ft_ls_tests/testsv2/09_test_opt_l.spec.c
+++ ft_ls_tests/testsv2/09_test_opt_l.spec.c
@@ -22,13 +22,33 @@ UT_TEST(09_test_opt_l)
 	sandbox_cmd("touch .a");
 	sandbox_cmd("dd bs=2 count=14450 if=/dev/random of=.a  >/dev/null 2>&1");
 	sandbox_cmd("ln -s .a b");
-/*
-	printf("\n=====  ls  ========\n");
-	printf("%s", ls(cmd));
-	printf("===== ft ls =======\n");
-	printf("%s", ft_ls(cmd));
-	printf("==================\n");
-*/
+	UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+
+
+	reset_sandbox();
+	cmd = "-la dir";
+	sandbox_cmd("mkdir -p dir/.hdir");
+	sandbox_cmd("touch dir/.hdir/file");
+
+	UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+
+
+	reset_sandbox();
+	cmd = "-l dir";
+	sandbox_cmd("mkdir -p dir");
+	sandbox_cmd("touch dir/.file touch dir/visible");
+	sandbox_cmd("ln dir/.file sym0");
+	sandbox_cmd("ln dir/.file sym1");
+	sandbox_cmd("ln dir/.file sym2");
+	sandbox_cmd("ln dir/.file sym3");
+	sandbox_cmd("ln dir/.file sym4");
+	sandbox_cmd("ln dir/.file sym5");
+	sandbox_cmd("ln dir/.file sym6");
+	sandbox_cmd("ln dir/.file sym7");
+	sandbox_cmd("ln dir/.file sym8");
+	sandbox_cmd("ln dir/.file sym9");
+	sandbox_cmd("ln dir/.file sym10");
+
 	UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 	/*

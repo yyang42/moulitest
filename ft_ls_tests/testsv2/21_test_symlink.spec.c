bf183a6d583bbf14baa2c1af75d0f2409f6deca5
yyang <yan.yaang@gmail.com>
2014-12-09 18:25:57 +0100

    update sym multi

--- ft_ls_tests/testsv2/21_test_symlink.spec.c
+++ ft_ls_tests/testsv2/21_test_symlink.spec.c
@@ -7,24 +7,19 @@ UT_TEST(21_test_symlink)
 	cmd = "-1 symdir";
 	reset_sandbox();
 	sandbox_cmd("mkdir mydir && ln -s mydir symdir && touch mydir/file{1..5}");
-
-
-	printf("\n========== ls ==========\n");
-	printf(ls(cmd));
-	printf("========= ft_ls ========\n");
-	printf(ft_ls(cmd));
-	printf("========================\n");
 	UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 	reset_sandbox();
 	sandbox_cmd("touch a");
 	sandbox_cmd("ln -s a b");
 	cmd = "-1 b";
-	printf("\n========== ls ==========\n");
-	printf(ls(cmd));
-	printf("========= ft_ls ========\n");
-	printf(ft_ls(cmd));
-	printf("========================\n");
+	UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+
+	reset_sandbox();
+	sandbox_cmd("mkdir a");
+	sandbox_cmd("ln -s a b");
+	sandbox_cmd("rm -rf a");
+	cmd = "-1 b";
 	UT_ASSERT(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 	// reset_sandbox();

9b7a1fe2f8779b23427a09a5ffda5811301ebcb1
Ilann <iadjedj@student.42.fr>
2014-12-10 18:44:19 +0100

    Add standard input test for GNL
    if fd <= 0 when checking errors instead of fd < 0

--- get_next_line_tests/tests/05_test_error_handling.spec.c
+++ get_next_line_tests/tests/05_test_error_handling.spec.c
@@ -6,6 +6,7 @@ UT_ASSERT_EQ(get_next_line(99, NULL), -1);
 char 	*line;
 UT_ASSERT_EQ(get_next_line(-99, &line), -1);
 UT_ASSERT_EQ(get_next_line(-1, &line), -1);
+UT_ASSERT_NEQ(get_next_line(0, &line), -1);
 
 /* Not opened fd */
 UT_ASSERT_EQ(get_next_line(42, &line), -1);

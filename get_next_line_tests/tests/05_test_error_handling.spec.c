12e7645e2940fb220dd33f07f2abbd581660a083
Cedric LEGRAND <celegran@student.42.fr>
2014-12-12 18:24:25 +0100

    redo some gnl changes lost in wrong commit

--- get_next_line_tests/tests/05_test_error_handling.spec.c
+++ get_next_line_tests/tests/05_test_error_handling.spec.c
@@ -7,7 +7,7 @@ UT_TEST(05_test_error_handling)
 	UT_ASSERT_EQ(get_next_line(1, NULL), -1);
 	UT_ASSERT_EQ(get_next_line(99, NULL), -1);
 
-	char 	*line;
+	char 	*line = NULL;
 	UT_ASSERT_EQ(get_next_line(-99, &line), -1);
 	UT_ASSERT_EQ(get_next_line(-1, &line), -1);
 	// UT_ASSERT_NEQ(get_next_line(0, &line), -1);

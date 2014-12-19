ad481627c2566db286f326cc753e5182a226f454
yyang <yan.yaang@gmail.com>
2014-12-19 10:37:54 +0100

    ft_ls fix test 51

--- ft_ls_tests/testsv2/51_hard_test_dev.spec.c
+++ ft_ls_tests/testsv2/51_hard_test_dev.spec.c
@@ -2,13 +2,18 @@
 
 UT_TEST(51_hard_test_dev)
 {
+	char *cmd;
 	reset_sandbox();
 
-	UT_ASSERT_W(strcmp(ls("-1 /dev"), ft_ls("-1 /dev")) == 0);
+	cmd = "-1 /dev";
+	UT_ASSERT_W(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 	/* io8 ignored because its minor device is not stable */
-	UT_ASSERT_W(strcmp(ls("-1l /dev | grep -v io8"), ft_ls("-1l /dev | grep -v io8")) == 0);
-	UT_ASSERT_W(strcmp(ls("-1la /dev | grep -v io8"), ft_ls("-1la /dev | grep -v io8")) == 0);
+
+	cmd = "-1l /dev | grep -v io8 | grep -v autofs_nowait | sed -E \"s/ +/ /g\"";
+	UT_ASSERT_W(strcmp(ls(cmd), ft_ls(cmd)) == 0);
+	cmd = "-1la /dev | grep -v io8 | grep -v autofs_nowait | sed -E \"s/ +/ /g\"";
+	UT_ASSERT_W(strcmp(ls(cmd), ft_ls(cmd)) == 0);
 
 	/*
 	printf("\n===================\n");

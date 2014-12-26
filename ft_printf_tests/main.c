05ed0061d1ea558aa8bf32904b64def7820e3870
yyang <yan.yaang@gmail.com>
2014-12-26 16:19:12 +0100

    v3: working implementation for printf. not finished yet

--- ft_printf_tests/main.c
+++ ft_printf_tests/main.c
@@ -5,6 +5,7 @@
 #include <test.h>
 #include <signal.h>
 
+// # define MT_ADD_SUITE_PREF
 # define MT_ADD_PROTO(name)			void suite_ ## name(t_suite *suite)
 
 PROTOTYPES
@@ -13,7 +14,8 @@ int main()
 {
 	t_mt	*mt = mt_create();
 
-	MT_ADD_SUITE(mt, suite_00_no_modifier);
+	ADD_TESTS
+	// MT_ADD_SUITE(mt, suite_00_no_modifier);
 
 	mt_exec(mt);
 	return(0);

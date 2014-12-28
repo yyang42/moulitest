106275247741e97d0712d80f57f6a90ad297862d
yyang <yan.yaang@gmail.com>
2014-12-28 12:25:47 +0100

    remove lst arg from lst_create_elem

--- testframework/v3/includes/fw.h
+++ testframework/v3/includes/fw.h
@@ -5,7 +5,7 @@
 # include <mt.h>
 
 #define assert(cond) test_assert_prep(test, #cond); test_assert(test, (cond))
-#define MT_ADD_SUITE(mt, name, suite_fn) lst_push(mt->suites, lst_create_elem(mt->suites, suite_create(#name, suite_fn)))
-#define SUITE_ADD_TEST(suite, test_fn) lst_push(suite->tests, lst_create_elem(suite->tests, test_create(#test_fn, test_fn)))
+#define MT_ADD_SUITE(mt, name, suite_fn) lst_push(mt->suites, lst_create_elem(suite_create(#name, suite_fn)))
+#define SUITE_ADD_TEST(suite, test_fn) lst_push(suite->tests, lst_create_elem(test_create(#test_fn, test_fn)))
 
 #endif

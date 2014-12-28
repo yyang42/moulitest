627bd8f88ae559ba2f06489340ee9814ae85b759
yyang <yan.yaang@gmail.com>
2014-12-28 18:53:10 +0100

    add more tests tests/03_modifier_d.spec.c

--- ft_printf_tests/tests/03_modifier_d.spec.c
+++ ft_printf_tests/tests/03_modifier_d.spec.c
@@ -2,12 +2,20 @@
 
 static void test_digit(t_test *test)
 {
-	// debug_on();
+	debug_on();
 	assert_printf("%d", 42);
-	// debug_off();
+	debug_off();
+}
+
+static void test_digit_with_strings(t_test *test)
+{
+	debug_on();
+	assert_printf("before %d after", 42);
+	debug_off();
 }
 
 void	suite_03_modifier_d(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_digit);
+	SUITE_ADD_TEST(suite, test_digit_with_strings);
 }

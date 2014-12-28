b143342131666a60f31b3e2a8b6006d1e27e2249
yyang <yan.yaang@gmail.com>
2014-12-28 18:56:28 +0100

    printf update tests/03_modifier_d.spec.c

--- ft_printf_tests/tests/03_modifier_d.spec.c
+++ ft_printf_tests/tests/03_modifier_d.spec.c
@@ -2,20 +2,38 @@
 
 static void test_digit(t_test *test)
 {
-	debug_on();
+	// debug_on();
 	assert_printf("%d", 42);
-	debug_off();
+	// debug_off();
 }
 
 static void test_digit_with_strings(t_test *test)
 {
-	debug_on();
+	// debug_on();
 	assert_printf("before %d after", 42);
-	debug_off();
+	// debug_off();
+}
+
+static void test_many_digits(t_test *test)
+{
+	// debug_on();
+	assert_printf("%d%d%d%d%d",
+		1, 2, 3, 4, 5);
+	// debug_off();
+}
+
+static void test_many_digits_width_strings(t_test *test)
+{
+	// debug_on();
+	assert_printf("a%db%dc%dd",
+		1, 2, 3);
+	// debug_off();
 }
 
 void	suite_03_modifier_d(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_digit);
 	SUITE_ADD_TEST(suite, test_digit_with_strings);
+	SUITE_ADD_TEST(suite, test_many_digits);
+	SUITE_ADD_TEST(suite, test_many_digits_width_strings);
 }

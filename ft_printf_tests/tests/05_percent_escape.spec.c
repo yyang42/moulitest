90245d76e34225b8e1443e138b453489feeb2e81
yyang <yan.yaang@gmail.com>
2014-12-28 21:33:17 +0100

    printf: handle %%

---
+++ ft_printf_tests/tests/05_percent_escape.spec.c
@@ -0,0 +1,37 @@
+#include <project.h>
+
+static void test_percent_alone(t_test *test)
+{
+	// debug_on();
+	assert_printf_noarg("%%");
+	// debug_off();
+}
+
+static void test_percent_simple(t_test *test)
+{
+	// debug_on();
+	assert_printf_noarg("aa%%bb");
+	// debug_off();
+}
+
+static void test_many_percent(t_test *test)
+{
+	// debug_on();
+	assert_printf_noarg("%%%%%%%%%%");
+	// debug_off();
+}
+
+static void test_more_percent(t_test *test)
+{
+	// debug_on();
+	assert_printf_noarg(".%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.%%.");
+	// debug_off();
+}
+
+void	suite_05_percent_escape(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_percent_alone);
+	SUITE_ADD_TEST(suite, test_percent_simple);
+	SUITE_ADD_TEST(suite, test_many_percent);
+	SUITE_ADD_TEST(suite, test_more_percent);
+}

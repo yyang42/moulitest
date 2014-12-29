3bfaf62ab80452893cdf350c3e4694f4eca10e19
yyang <yan.yaang@gmail.com>
2014-12-29 20:35:42 +0100

    printf: add S

--- ft_printf_tests/tests/07_conv_S.spec.c
+++ ft_printf_tests/tests/07_conv_S.spec.c
@@ -2,11 +2,34 @@
 
 static void test_simple(t_test *test)
 {
-	debug_next_assert();
+	// debug_next_assert();
 	assert_printf("%S", L"米");
+}
+
+static void test_multiple_wchars(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%S", L"我是一只猫。");
+}
+
+static void test_greek_alphabet_wchars(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S%S",
+	L"Α α", L"Β β", L"Γ γ", L"Δ δ", L"Ε ε", L"Ζ ζ", L"Η η", L"Θ θ", L"Ι ι", L"Κ κ", L"Λ λ", L"Μ μ",
+	L"Ν ν", L"Ξ ξ", L"Ο ο", L"Π π", L"Ρ ρ", L"Σ σ", L"Τ τ", L"Υ υ", L"Φ φ", L"Χ χ", L"Ψ ψ", L"Ω ω", L"");
+}
+
+static void test_mix(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("a%Sb%sc%S", L"我", "42", L"猫");
 }
 
 void	suite_07_conv_S(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_simple);
+	SUITE_ADD_TEST(suite, test_multiple_wchars);
+	SUITE_ADD_TEST(suite, test_greek_alphabet_wchars);
+	SUITE_ADD_TEST(suite, test_mix);
 }

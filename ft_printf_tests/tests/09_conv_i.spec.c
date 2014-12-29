c09498a905f4191e3cc80d373ec25f1d15b6ad53
yyang <yan.yaang@gmail.com>
2014-12-29 21:24:13 +0100

    printf: add conv i

---
+++ ft_printf_tests/tests/09_conv_i.spec.c
@@ -0,0 +1,42 @@
+#include <project.h>
+
+static void test_digit(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%i", 42);
+}
+
+static void test_digit_negative(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%i", -42);
+}
+
+static void test_digit_with_strings(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("before %i after", 42);
+}
+
+static void test_many_digits(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%i%i%i%i%i",
+		1, -2, 3, -4, 5);
+}
+
+static void test_many_digits_width_strings(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("a%ib%ic%id",
+		1, -2, 3);
+}
+
+void	suite_09_conv_i(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_digit);
+	SUITE_ADD_TEST(suite, test_digit_negative);
+	SUITE_ADD_TEST(suite, test_digit_with_strings);
+	SUITE_ADD_TEST(suite, test_many_digits);
+	SUITE_ADD_TEST(suite, test_many_digits_width_strings);
+}

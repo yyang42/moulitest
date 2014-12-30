16f9652de8bc4afa260b5c515e23b2dfc0944a29
yyang <yan.yaang@gmail.com>
2014-12-30 15:52:22 +0100

    test-printf: add 18_mix_all_previous.spec.c

---
+++ ft_printf_tests/tests/18_mix_all_previous.spec.c
@@ -0,0 +1,22 @@
+#include <project.h>
+#include <limits.h>
+
+static void test_simple(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%s %C %d %p %x %% %S", "bonjour ", L'該', 42, &free, 42, L"لحم خنزير");
+}
+
+static void test_hard(t_test *test)
+{
+	// debug_next_assert();
+	char c;
+	assert_printf("%s%d%p%%%S%D%i%o%O%u%U%x%X%c%C","bonjour", 42, &c, L"暖炉", LONG_MAX, 42, 42, 42, 100000, ULONG_MAX, 42, 42, 'c', L'플');
+}
+
+void	suite_18_mix_all_previous(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_simple);
+	SUITE_ADD_TEST(suite, test_hard);
+
+}

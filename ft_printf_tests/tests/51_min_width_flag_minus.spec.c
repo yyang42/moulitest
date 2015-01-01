2c05fae43f7554a6848fe5a9369c7507d0421e48
yyang <yan.yaang@gmail.com>
2015-01-01 17:18:40 +0100

    printf: handle left/right justification 2

--- ft_printf_tests/tests/51_min_width_flag_minus.spec.c
+++ ft_printf_tests/tests/51_min_width_flag_minus.spec.c
@@ -20,7 +20,7 @@ static void test_min_width_larger_than_input(t_test *test)
 	assert_printf("{%-30d}", 10000);
 }
 
-void	suite_51_min_width_left_flag(t_suite *suite)
+void	suite_51_min_width_flag_minus(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_simple);
 	SUITE_ADD_TEST(suite, test_min_width_smaller_than_input);

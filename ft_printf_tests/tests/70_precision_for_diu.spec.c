2e7402f542f426f2d5a852c54a37d7aacffb6e84
yyang <yan.yaang@gmail.com>
2015-01-01 18:45:05 +0100

    printf: handle precision for diu

--- ft_printf_tests/tests/70_precision_for_diu.spec.c
+++ ft_printf_tests/tests/70_precision_for_diu.spec.c
@@ -50,43 +50,31 @@ static void test_precision_i_zero_value(t_test *test)
 	assert_printf("%.i, %.0i", 0, 0);
 }
 
-// static void test_precision_empty(t_test *test)
-// {
-// 	debug_next_assert();
-// 	assert_printf("%.d", 0);
-// }
+static void test_precision_u(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%.4u", 42);
+}
 
-// static void test_precision_zero(t_test *test)
-// {
-// 	debug_next_assert();
-// 	assert_printf("%.0d", 0);
-// }
+static void test_precision_u_higher_min_width(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%15.4u", 42);
+}
 
-// static void test_precision_octal(t_test *test)
-// {
-// 	debug_next_assert();
-// 	assert_printf("%#.o", 42);
-// }
+static void test_precision_u_higher_precision(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%4.15u", 42);
+}
 
-// static void test_precision_octal_zero(t_test *test)
-// {
-// 	debug_next_assert();
-// 	assert_printf("%#.o", 0);
-// }
+static void test_precision_u_zero_value(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("%.u, %.0u", 0, 0);
+}
 
-// static void test_precision_hex(t_test *test)
-// {
-// 	debug_next_assert();
-// 	assert_printf("%#.x", 42);
-// }
-
-// static void test_precision_hex_zero(t_test *test)
-// {
-// 	debug_next_assert();
-// 	assert_printf("%#.x", 0);
-// }
-
-void	suite_70_precision_for_di(t_suite *suite)
+void	suite_70_precision_for_diu(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_precision_d);
 	SUITE_ADD_TEST(suite, test_precision_d_higher_min_width);
@@ -96,10 +84,8 @@ void	suite_70_precision_for_di(t_suite *suite)
 	SUITE_ADD_TEST(suite, test_precision_i_higher_min_width);
 	SUITE_ADD_TEST(suite, test_precision_i_higher_precision);
 	SUITE_ADD_TEST(suite, test_precision_i_zero_value);
-	// SUITE_ADD_TEST(suite, test_precision_empty);
-	// SUITE_ADD_TEST(suite, test_precision_zero);
-	// SUITE_ADD_TEST(suite, test_precision_octal);
-	// SUITE_ADD_TEST(suite, test_precision_octal_zero);
-	// SUITE_ADD_TEST(suite, test_precision_hex);
-	// SUITE_ADD_TEST(suite, test_precision_hex_zero);
+	SUITE_ADD_TEST(suite, test_precision_u);
+	SUITE_ADD_TEST(suite, test_precision_u_higher_min_width);
+	SUITE_ADD_TEST(suite, test_precision_u_higher_precision);
+	SUITE_ADD_TEST(suite, test_precision_u_zero_value);
 }

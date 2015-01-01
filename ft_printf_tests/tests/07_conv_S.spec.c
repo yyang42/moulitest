585f28948a8387e5c102921defc7de0097110dd4
yyang <yan.yaang@gmail.com>
2015-01-01 22:02:23 +0100

    printf tests: update todo

--- ft_printf_tests/tests/07_conv_S.spec.c
+++ ft_printf_tests/tests/07_conv_S.spec.c
@@ -20,6 +20,19 @@ static void test_greek_alphabet_wchars(t_test *test)
 	L"Ν ν", L"Ξ ξ", L"Ο ο", L"Π π", L"Ρ ρ", L"Σ σ", L"Τ τ", L"Υ υ", L"Φ φ", L"Χ χ", L"Ψ ψ", L"Ω ω", L"");
 }
 
+// static void test_new(t_test *test)
+// {
+// 	wchar_t wstr[5];
+
+// 	wstr[0] = L'A';
+// 	wstr[1] = 945;
+// 	wstr[2] = 0x801;
+// 	wstr[3] = 0x10903;
+// 	wstr[4] = L'\0';
+// 	debug_next_assert();
+// 	assert_printf("%S", wstr);
+// }
+
 static void test_mix(t_test *test)
 {
 	// debug_next_assert();
@@ -31,5 +44,6 @@ void	suite_07_conv_S(t_suite *suite)
 	SUITE_ADD_TEST(suite, test_simple);
 	SUITE_ADD_TEST(suite, test_multiple_wchars);
 	SUITE_ADD_TEST(suite, test_greek_alphabet_wchars);
+	// SUITE_ADD_TEST(suite, test_new);
 	SUITE_ADD_TEST(suite, test_mix);
 }
--- ft_printf_tests/tests/todo.c
+++ ft_printf_tests/tests/todo.c
@@ -2,18 +2,13 @@
 
 static void todo(t_test *test)
 {
-	// error handling
-	// assert_printf("%o", -42);
-	// assert_printf("%o", LONG_MAX);
-	// assert_printf("%O", LONG_MIN);
-	// assert_printf("%u", -42);
-	// assert_printf("%u", UINT_MAX + 1);
-	// assert_printf("%U", ULONG_MAX + 1);
-	// assert_printf("%c", 200);
-	assert_printf("%hhd", SHRT_MAX);
-	assert_printf("%zi", LLONG_MIN);
-	assert_printf("%zd", LLONG_MIN);
-	assert_printf("%+o", 42);
-	assert_printf("%#.o", 42);
-	assert_printf("%#+12.45llp", 42645454564);
+/*
+	UT_PRINTF_ASSERT("%#O\n", 042);
+	UT_PRINTF_ASSERT("%.5d\n", -42);
+	UT_PRINTF_ASSERT("%05d\n", -42);
+
+	UT_PRINTF_ASSERT("%.5c\n", 'c');
+	UT_PRINTF_ASSERT("%.25p\n", &strlen);
+	UT_PRINTF_ASSERT("%025p\n", &strlen);
+*/
 }

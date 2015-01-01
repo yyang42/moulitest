495383496c86fe03acb37c8a32be24ae32b0dc5a
yyang <yan.yaang@gmail.com>
2015-01-01 17:59:30 +0100

    printf: handle flag space

---
+++ ft_printf_tests/tests/62_flag_space.spec.c
@@ -0,0 +1,35 @@
+#include <project.h>
+#include <limits.h>
+#include <signal.h>
+
+static void test_space_d_simple(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("% d", 42);
+}
+
+static void test_space_d_simple_minus(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("% d", -42);
+}
+
+static void test_space_i_simple(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("% i", 42);
+}
+
+static void test_space_i_simple_minus(t_test *test)
+{
+	// debug_next_assert();
+	assert_printf("% i", -42);
+}
+
+void	suite_62_flag_space(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_space_d_simple);
+	SUITE_ADD_TEST(suite, test_space_d_simple_minus);
+	SUITE_ADD_TEST(suite, test_space_i_simple);
+	SUITE_ADD_TEST(suite, test_space_i_simple_minus);
+}

63ad20417d227df540da1d04c0961f22ab673781
yyang <yan.yaang@gmail.com>
2015-01-02 19:18:30 +0100

    printf: fix min_width

--- ft_printf_tests/tests/50_min_width.spec.c
+++ ft_printf_tests/tests/50_min_width.spec.c
@@ -2,27 +2,34 @@
 #include <limits.h>
 #include <signal.h>
 
-static void test_simple(t_test *test)
+static void simple(t_test *test)
 {
 	// test->debug = 1;
 	assert_printf("{%10d}", 42);
 }
 
-static void test_min_width_smaller_than_input(t_test *test)
+static void min_width_smaller_than_input(t_test *test)
 {
 	// test->debug = 1;
 	assert_printf("{%4d}", 10000);
 }
 
-static void test_min_width_larger_than_input(t_test *test)
+static void min_width_larger_than_input(t_test *test)
 {
 	// test->debug = 1;
 	assert_printf("{%30d}", 10000);
 }
 
+static void negative_width(t_test *test)
+{
+	test->debug = 1;
+	assert_printf("{%10d}", -42);
+}
+
 void	suite_50_min_width(t_suite *suite)
 {
-	SUITE_ADD_TEST(suite, test_simple);
-	SUITE_ADD_TEST(suite, test_min_width_smaller_than_input);
-	SUITE_ADD_TEST(suite, test_min_width_larger_than_input);
+	SUITE_ADD_TEST(suite, simple);
+	SUITE_ADD_TEST(suite, min_width_smaller_than_input);
+	SUITE_ADD_TEST(suite, min_width_larger_than_input);
+	SUITE_ADD_TEST(suite, negative_width);
 }

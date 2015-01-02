bccb820ba3529d75946463099bf5056ca8887165
yyang <yan.yaang@gmail.com>
2015-01-02 18:47:33 +0100

    framework: cealn ui

--- testframework/v3/suite/suite_exec.c
+++ testframework/v3/suite/suite_exec.c
@@ -35,11 +35,11 @@ static void		suite_print_first_failure(t_suite *suite)
 	if (!elem)
 		return ;
 	test = elem->data;
-	printf(" ");
-	fprintf(stdout, C_RED"%s -> \"%s\""C_CLEAR,
-		test->name,
-		test->last_assert_cond);
 	printf(C_RED" [%s]"C_CLEAR, test_get_failure_type(test));
+	fprintf(stdout, C_RED" %s"C_CLEAR, test->name);
+	// fprintf(stdout, C_RED"%s -> \"%s\""C_CLEAR,
+	// 	test->name,
+	// 	test->last_assert_cond);
 }
 
 static void		suite_print_suite_summary(t_suite *suite)

19c58b443f9f51e6ae4ac1d6a81d615befd20122
yyang <yan.yaang@gmail.com>
2015-01-02 20:25:26 +0100

    fix failure msg

--- testframework/v3/test/test_get_failure_type.c
+++ testframework/v3/test/test_get_failure_type.c
@@ -11,6 +11,8 @@ char	*test_get_failure_type(t_test *test)
 		return (strdup("SEGV"));
 	else if (test->sig == SIGBUS)
 		return (strdup("BUS!"));
+	else if (test->sig == SIGALRM)
+		return (strdup("TIME"));
 	else
 		return (strdup("OK !"));
 }

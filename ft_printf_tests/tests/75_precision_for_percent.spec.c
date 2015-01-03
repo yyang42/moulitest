f18b3fa0566f366e2636fe7586896913999edad7
yyang <yan.yaang@gmail.com>
2015-01-03 17:47:46 +0100

    printf: add tests precision percent

---
+++ ft_printf_tests/tests/75_precision_for_percent.spec.c
@@ -0,0 +1,14 @@
+#include <project.h>
+#include <limits.h>
+#include <signal.h>
+
+static void percent_point3precision(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf_noarg("%.3%");
+}
+
+void	suite_75_precision_for_percent(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, percent_point3precision);
+}

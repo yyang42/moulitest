7d48ee26c0716bc9b133511936cc0b9ae48d4e42
yyang <yan.yaang@gmail.com>
2015-01-03 20:07:50 +0100

    printf: add test suite_69_flag_mix

---
+++ ft_printf_tests/tests/69_flag_mix.spec.c
@@ -0,0 +1,21 @@
+#include <project.h>
+#include <limits.h>
+#include <signal.h>
+
+static void positiveDigit_flagsSpacePlus_spaceFirst(t_test *test)
+{
+	test->debug = 1;
+	assert_printf("{% +d}", 42);
+}
+
+static void positiveDigit_flagsSpacePlus_plusFirst(t_test *test)
+{
+	test->debug = 1;
+	assert_printf("{%+ d}", 42);
+}
+
+void	suite_69_flag_mix(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, positiveDigit_flagsSpacePlus_spaceFirst);
+	SUITE_ADD_TEST(suite, positiveDigit_flagsSpacePlus_plusFirst);
+}

e7dc7592e8b082fab1fae6f5473b97add20118ca
yyang <yan.yaang@gmail.com>
2015-01-03 22:23:56 +0100

    printf: fix char case {%03c}

--- ft_printf_tests/tests/52_min_width_flag_zero.spec.c
+++ ft_printf_tests/tests/52_min_width_flag_zero.spec.c
@@ -25,10 +25,17 @@ static void test_min_width_larger_than_input(t_test *test)
 	assert_printf("{%030d}", 10000);
 }
 
+static void char_posMinWidth_zeroFlag(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("{%03c}", 0);
+}
+
 void	suite_52_min_width_flag_zero(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_simple);
 	SUITE_ADD_TEST(suite, test_negative_value);
 	SUITE_ADD_TEST(suite, test_min_width_smaller_than_input);
 	SUITE_ADD_TEST(suite, test_min_width_larger_than_input);
+	SUITE_ADD_TEST(suite, char_posMinWidth_zeroFlag);
 }

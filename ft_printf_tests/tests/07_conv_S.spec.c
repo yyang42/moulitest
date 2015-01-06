9e471edd1f2843bf610e861d4d8abfb9c84d69ef
yyang <yan.yaang@gmail.com>
2015-01-06 19:17:24 +0100

    print: add test ("{%S}", NULL)

--- ft_printf_tests/tests/07_conv_S.spec.c
+++ ft_printf_tests/tests/07_conv_S.spec.c
@@ -26,6 +26,12 @@ static void test_mix(t_test *test)
 	assert_printf("a%Sb%sc%S", L"我", "42", L"猫");
 }
 
+static void nullWideString(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("{%S}", NULL);
+}
+
 void	suite_07_conv_S(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_simple);
@@ -33,4 +39,5 @@ void	suite_07_conv_S(t_suite *suite)
 	SUITE_ADD_TEST(suite, test_greek_alphabet_wchars);
 	// SUITE_ADD_TEST(suite, test_new);
 	SUITE_ADD_TEST(suite, test_mix);
+	SUITE_ADD_TEST(suite, nullWideString);
 }

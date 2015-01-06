ee876162bfc30cfe969d7742839ab10203dce0a5
yyang <yan.yaang@gmail.com>
2015-01-06 18:24:28 +0100

    prinft: more tests for C

--- ft_printf_tests/tests/73_precision_for_cC.spec.c
+++ ft_printf_tests/tests/73_precision_for_cC.spec.c
@@ -20,9 +20,30 @@ static void cPositiveValue_positivePrecision(t_test *test)
 	assert_printf("%.5c", 42);
 }
 
+static void cUpperNullChar(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%.C", 0);
+}
+
+static void cUpperNullChar_positivePrecision(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%.5C", 0);
+}
+
+static void cUpperPositiveValue_positivePrecision(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%.5C", 42);
+}
+
 void	suite_73_precision_for_cC(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, cNullChar);
 	SUITE_ADD_TEST(suite, cNullChar_positivePrecision);
 	SUITE_ADD_TEST(suite, cPositiveValue_positivePrecision);
+	SUITE_ADD_TEST(suite, cUpperNullChar);
+	SUITE_ADD_TEST(suite, cUpperNullChar_positivePrecision);
+	SUITE_ADD_TEST(suite, cUpperPositiveValue_positivePrecision);
 }

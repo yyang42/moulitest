098bddd15313788c79719d7cb088d146442f3084
yyang <yan.yaang@gmail.com>
2015-01-06 19:04:46 +0100

    printf: add S

---
+++ ft_printf_tests/tests/72_precision_for_sS.spec.c
@@ -0,0 +1,49 @@
+#include <project.h>
+#include <limits.h>
+#include <signal.h>
+
+static void test_precision_s(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%.4s", "42");
+}
+
+static void test_precision_s_higher_min_width(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%15.4s", "42");
+}
+
+static void test_precision_s_higher_precision(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%4.15s", "42");
+}
+
+static void test_precision_S(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%.4S", L"我是一只猫。");
+}
+
+static void test_precision_S_higher_min_width(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%15.4S", L"我是一只猫。");
+}
+
+static void test_precision_S_higher_precision(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%4.15S", L"我是一只猫。");
+}
+
+void	suite_72_precision_for_sS(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, test_precision_s);
+	SUITE_ADD_TEST(suite, test_precision_s_higher_min_width);
+	SUITE_ADD_TEST(suite, test_precision_s_higher_precision);
+	SUITE_ADD_TEST(suite, test_precision_S);
+	SUITE_ADD_TEST(suite, test_precision_S_higher_min_width);
+	SUITE_ADD_TEST(suite, test_precision_S_higher_precision);
+}

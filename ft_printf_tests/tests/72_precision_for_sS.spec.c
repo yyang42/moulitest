ba496ae48b8b74b85543195d079ccd98f42276c6
yyang <yan.yaang@gmail.com>
2015-01-06 19:10:22 +0100

    printf: add S for real

--- ft_printf_tests/tests/72_precision_for_sS.spec.c
+++ ft_printf_tests/tests/72_precision_for_sS.spec.c
@@ -38,6 +38,12 @@ static void test_precision_S_higher_precision(t_test *test)
 	assert_printf("%4.15S", L"我是一只猫。");
 }
 
+static void test_precision_S_higher_precision_ascii(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%4.1S", L"Jambon");
+}
+
 void	suite_72_precision_for_sS(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_precision_s);
@@ -46,4 +52,5 @@ void	suite_72_precision_for_sS(t_suite *suite)
 	SUITE_ADD_TEST(suite, test_precision_S);
 	SUITE_ADD_TEST(suite, test_precision_S_higher_min_width);
 	SUITE_ADD_TEST(suite, test_precision_S_higher_precision);
+	SUITE_ADD_TEST(suite, test_precision_S_higher_precision_ascii);
 }

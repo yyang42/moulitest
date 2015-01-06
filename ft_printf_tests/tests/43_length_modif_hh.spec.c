c305be615076dd00cbd540dca804d9412027ede0
yyang <yan.yaang@gmail.com>
2015-01-06 20:19:20 +0100

    add more tests for hh

--- ft_printf_tests/tests/43_length_modif_hh.spec.c
+++ ft_printf_tests/tests/43_length_modif_hh.spec.c
@@ -14,10 +14,22 @@ static void test_hhd_max(t_test *test)
 	assert_printf("%hhd", CHAR_MAX);
 }
 
+static void test_hhd_max_plus(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%hhd", SHRT_MAX);
+}
+
 static void test_hhd_min(t_test *test)
 {
 	// test->debug = 1;
 	assert_printf("%hhd", CHAR_MIN);
+}
+
+static void test_hhd_min_minus(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%hhd", SHRT_MIN);
 }
 
 static void test_hhi_simple(t_test *test)
@@ -32,10 +44,22 @@ static void test_hhi_max(t_test *test)
 	assert_printf("%hhd", CHAR_MAX);
 }
 
+static void test_hhi_max_plus(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%hhd", SHRT_MAX);
+}
+
 static void test_hhi_min(t_test *test)
 {
 	// test->debug = 1;
 	assert_printf("%hhi", CHAR_MIN);
+}
+
+static void test_hhi_min_minus(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%hhi", SHRT_MIN);
 }
 
 static void test_hhu_max(t_test *test)
@@ -62,18 +86,50 @@ static void test_hhX_max(t_test *test)
 	assert_printf("%hhX, %hhX", 0, UCHAR_MAX);
 }
 
+static void test_hhu_max_plus(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%hhu, %hhu", 0, USHRT_MAX);
+}
+
+static void test_hho_max_plus(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%hho, %hho", 0, USHRT_MAX);
+}
+
+static void test_hhx_max_plus(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%hhx, %hhx", 0, USHRT_MAX);
+}
+
+static void test_hhX_max_plus(t_test *test)
+{
+	// test->debug = 1;
+	assert_printf("%hhX, %hhX", 0, USHRT_MAX);
+}
+
 void	suite_43_length_modif_hh(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_hhd_simple);
 	SUITE_ADD_TEST(suite, test_hhd_max);
+	SUITE_ADD_TEST(suite, test_hhd_max_plus);
 	SUITE_ADD_TEST(suite, test_hhd_min);
+	SUITE_ADD_TEST(suite, test_hhd_min_minus);
 
 	SUITE_ADD_TEST(suite, test_hhi_simple);
 	SUITE_ADD_TEST(suite, test_hhi_max);
+	SUITE_ADD_TEST(suite, test_hhi_max_plus);
 	SUITE_ADD_TEST(suite, test_hhi_min);
+	SUITE_ADD_TEST(suite, test_hhi_min_minus);
 
 	SUITE_ADD_TEST(suite, test_hhu_max);
 	SUITE_ADD_TEST(suite, test_hho_max);
 	SUITE_ADD_TEST(suite, test_hhx_max);
 	SUITE_ADD_TEST(suite, test_hhX_max);
+	SUITE_ADD_TEST(suite, test_hhu_max_plus);
+	SUITE_ADD_TEST(suite, test_hho_max_plus);
+	SUITE_ADD_TEST(suite, test_hhx_max_plus);
+	SUITE_ADD_TEST(suite, test_hhX_max_plus);
 }

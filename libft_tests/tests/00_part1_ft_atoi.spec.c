adaa2dc4bfad40c06a4d5726b649d2a9c66925cd
yyang <yan.yaang@gmail.com>
2015-01-11 13:52:16 +0100

    libft: convert atoi & bzero

--- libft_tests/tests/00_part1_ft_atoi.spec.c
+++ libft_tests/tests/00_part1_ft_atoi.spec.c
@@ -1,54 +1,76 @@
 #include <project.h>
 
-#define mt_assert_atoi(str) mt_assert(ft_atoi(str) == atoi(str))
+#define mt_test_atoi(test_name, tested_str)										\
+	static void test_## test_name(t_test *test)								\
+	{																		\
+		mt_assert(ft_atoi(tested_str) == atoi(tested_str));								\
+	}
 
-static void test_misc(t_test *test)
-{
-	mt_assert(ft_atoi(" -sfecf") == atoi(" -sfecf"));
-	mt_assert(ft_atoi("") == atoi(""));
-	mt_assert(ft_atoi("+2798") == atoi("+2798"));
-	mt_assert(ft_atoi("+0089") == atoi("+0089"));
-	mt_assert(ft_atoi("a56") == atoi("a56"));
-	mt_assert(ft_atoi("      --s8") == atoi("      --s8"));
-	mt_assert(ft_atoi("0001020304") == atoi("0001020304"));
-	mt_assert(ft_atoi("000000000000000110") == atoi("000000000000000110"));
-	mt_assert(ft_atoi("-153") == atoi("-153"));
-	mt_assert(ft_atoi("+132") == atoi("+132"));
-	mt_assert(ft_atoi("++876") == atoi("++876"));
-	mt_assert(ft_atoi("--132") == atoi("--132"));
-	mt_assert(ft_atoi("dwbk ") == atoi("dwbk "));
-	mt_assert(ft_atoi(" 1") == atoi(" 1"));
-	mt_assert(ft_atoi("42jk ") == atoi("42jk "));
-	mt_assert(ft_atoi(" 21") == atoi(" 21"));
-	mt_assert(ft_atoi("     32 ") == atoi("     32 "));
-	mt_assert(ft_atoi("\n          42 24") == atoi("\n          42 24"));
-	mt_assert(ft_atoi("1-2") == atoi("1-2"));
-	mt_assert(ft_atoi("4+2") == atoi("4+2"));
-	mt_assert(ft_atoi("  +442") == atoi("  +442"));
-	mt_assert(ft_atoi("  -4232") == atoi("  -4232"));
-	mt_assert(ft_atoi("4,5") == atoi("4,5"));
-	mt_assert(ft_atoi("+") == atoi("+"));
-	mt_assert(ft_atoi("-") == atoi("-"));
-	mt_assert(ft_atoi("-+1") == atoi("-+1"));
-	mt_assert(ft_atoi("+-1") == atoi("+-1"));
-	mt_assert(ft_atoi("\200123") == atoi("\200123"));
-	mt_assert(ft_atoi("123\200") == atoi("123\200"));
-}
-
-static void test_trim(t_test *test)
-{
-	mt_assert_atoi("  \t\n  \r\r\v\f-899");
-}
-
-static void test_extrem_values(t_test *test)
-{
-	mt_assert_atoi("-2147483648");
-	mt_assert_atoi("2147483647");
-}
+mt_test_atoi(num1, " -sfecf");
+mt_test_atoi(num2, "");
+mt_test_atoi(num3, "+2798");
+mt_test_atoi(num4, "+0089");
+mt_test_atoi(num5, "a56");
+mt_test_atoi(num6, "      --s8");
+mt_test_atoi(num7, "0001020304");
+mt_test_atoi(num8, "000000000000000110");
+mt_test_atoi(num9, "-153");
+mt_test_atoi(num10, "+132");
+mt_test_atoi(num11, "++876");
+mt_test_atoi(num12, "--132");
+mt_test_atoi(num13, "dwbk ");
+mt_test_atoi(num14, " 1");
+mt_test_atoi(num15, "42jk ");
+mt_test_atoi(num16, " 21");
+mt_test_atoi(num17, "     32 ");
+mt_test_atoi(num18, "\n          42 24");
+mt_test_atoi(num19, "1-2");
+mt_test_atoi(num20, "4+2");
+mt_test_atoi(num21, "  +442");
+mt_test_atoi(num22, "  -4232");
+mt_test_atoi(num23, "4,5");
+mt_test_atoi(num24, "+");
+mt_test_atoi(num25, "-");
+mt_test_atoi(num26, "-+1");
+mt_test_atoi(num27, "+-1");
+mt_test_atoi(num28, "\200123");
+mt_test_atoi(num29, "123\200");
+mt_test_atoi(num30, "  \t\n  \r\r\v\f-899");
+mt_test_atoi(num31, "-2147483648");
+mt_test_atoi(num32, "2147483647");
 
 void	suite_00_part1_ft_atoi(t_suite *suite)
 {
-	SUITE_ADD_TEST(suite, test_misc);
-	SUITE_ADD_TEST(suite, test_trim);
-	SUITE_ADD_TEST(suite, test_extrem_values);
+	SUITE_ADD_TEST(suite, test_num1);
+	SUITE_ADD_TEST(suite, test_num2);
+	SUITE_ADD_TEST(suite, test_num3);
+	SUITE_ADD_TEST(suite, test_num4);
+	SUITE_ADD_TEST(suite, test_num5);
+	SUITE_ADD_TEST(suite, test_num6);
+	SUITE_ADD_TEST(suite, test_num7);
+	SUITE_ADD_TEST(suite, test_num8);
+	SUITE_ADD_TEST(suite, test_num9);
+	SUITE_ADD_TEST(suite, test_num10);
+	SUITE_ADD_TEST(suite, test_num11);
+	SUITE_ADD_TEST(suite, test_num12);
+	SUITE_ADD_TEST(suite, test_num13);
+	SUITE_ADD_TEST(suite, test_num14);
+	SUITE_ADD_TEST(suite, test_num15);
+	SUITE_ADD_TEST(suite, test_num16);
+	SUITE_ADD_TEST(suite, test_num17);
+	SUITE_ADD_TEST(suite, test_num18);
+	SUITE_ADD_TEST(suite, test_num19);
+	SUITE_ADD_TEST(suite, test_num20);
+	SUITE_ADD_TEST(suite, test_num21);
+	SUITE_ADD_TEST(suite, test_num22);
+	SUITE_ADD_TEST(suite, test_num23);
+	SUITE_ADD_TEST(suite, test_num24);
+	SUITE_ADD_TEST(suite, test_num25);
+	SUITE_ADD_TEST(suite, test_num26);
+	SUITE_ADD_TEST(suite, test_num27);
+	SUITE_ADD_TEST(suite, test_num28);
+	SUITE_ADD_TEST(suite, test_num29);
+	SUITE_ADD_TEST(suite, test_num30);
+	SUITE_ADD_TEST(suite, test_num31);
+	SUITE_ADD_TEST(suite, test_num32);
 }

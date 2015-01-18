7f527cfaad12c11953a317bab4f77403ffd6afb0
yyang <yan.yaang@gmail.com>
2015-01-18 15:42:44 +0100

    update Makefile

--- get_next_line_tests/Makefile_cfg.mk
+++ get_next_line_tests/Makefile_cfg.mk
@@ -1,3 +1,4 @@
 NAME = get_next_line_tests
 RENDU_PATH_KEY = GET_NEXT_LINE_PATH
 CC_LIBS = -lft -L$(RENDU_PATH)/libft
+CC_SOURCE_EXTRA = $(RENDU_PATH)/get_next_line.c
--- testframework/v3_templates/Makefile_template.mk
+++ testframework/v3_templates/Makefile_template.mk
@@ -46,7 +46,7 @@ ifneq ("$(wildcard $(LIBFT_HEADER_PATH))","")
 	LIBFT_HEADER_INCLUDE = -I $(shell dirname $(LIBFT_HEADER_PATH))
 endif
 CC_INCLUDES = -I . -I ../testframework/v3/includes $(LIBFT_HEADER_INCLUDE) -I $(RENDU_PATH) -I$(RENDU_PATH)/libft/includes
-CC_SOURCE = ./tests/*.spec.c main.c utils.c
+CC_SOURCE = ./tests/*.spec.c main.c utils.c $(CC_SOURCE_EXTRA)
 pattern ?= spec.c$$
 TEST_FILES = ls -1 tests | grep -e "$(pattern)" | grep -e "$(POST_PATTERN)"
 ADD_TESTS = $(shell $(TEST_FILES) | sed -E "s/(.*)\.spec\.c/MT_ADD_SUITE\(mt, \1, suite_\1);/g")

f755302e7d6bee925f07f8ab1db61df23b8fc7b8
yyang <yan.yaang@gmail.com>
2015-01-22 14:42:17 +0100

    travis: update gnl 5 remove pwd

--- get_next_line_tests/tests/41_hard_test_large_file.spec.c
+++ get_next_line_tests/tests/41_hard_test_large_file.spec.c
@@ -9,7 +9,6 @@ static void test01(t_test *test)
 	int	diff_file_size;
     
     system("mkdir -p sandbox");
-	system("pwd");
 	system("openssl rand -out sandbox/large_file.txt -base64 $(($(echo '2^19' | bc) * 3/4)) 2> /dev/null");
 
 	fd = open("sandbox/large_file.txt", O_RDONLY);

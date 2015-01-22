3ae37590070051470e624c41423cd3615a2bd19f
juschaef <juschaef@student.42.fr>
2015-01-22 13:55:45 +0100

    travis: test real gnl

--- testframework/fake_rendu/get_next_line/get_next_line.h
+++ testframework/fake_rendu/get_next_line/get_next_line.h
@@ -1,6 +1,13 @@
-#ifndef GNL
-# define GNL
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int get_next_line(int fd, char **line);
+# define BUFF_SIZE 7
+# define MAX_BUFF_SIZE 8385531
+# define MAX_PARALLEL_FILES 10000
+
+# include <unistd.h>
+# include <strings.h>
+
+int get_next_line(int const fd, char **line);
 
 #endif

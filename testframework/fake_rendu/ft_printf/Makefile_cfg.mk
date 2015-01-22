d8ad567a74e5803aa6da161d82c623a43757d5ca
yyang <yan.yaang@gmail.com>
2015-01-22 15:29:09 +0100

    travis: update libft printf

--- testframework/fake_rendu/ft_printf/Makefile_cfg.mk
+++ testframework/fake_rendu/ft_printf/Makefile_cfg.mk
@@ -6,23 +6,8 @@
 #    By: yyang <yyang@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/01/22 14:47:06 by yyang             #+#    #+#              #
-#    Updated: 2015/01/22 15:27:35 by yyang            ###   ########.fr        #
+#    Updated: 2015/01/22 15:03:35 by yyang            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-# Required variables: NAME
-include Makefile_cfg.mk
-
-all: $(NAME)
-
-$(NAME):
-	gcc -I includes -I . -c *.c
-	ar rcs $(NAME) *.o
-
-clean:
-	rm -rf *.o
-
-fclean: clean
-	rm -f $(NAME)
-
-re: fclean all
+NAME = libftprintf.a

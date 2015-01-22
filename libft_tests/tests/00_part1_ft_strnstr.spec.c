f81397b6ac6ca38fc94fdc34f2b700d7c1e62a69
yyang <yan.yaang@gmail.com>
2015-01-22 17:19:46 +0100

    travis: update 00_part1_ft_strnstr

--- libft_tests/tests/00_part1_ft_strnstr.spec.c
+++ libft_tests/tests/00_part1_ft_strnstr.spec.c
@@ -7,12 +7,12 @@ static void unittest1(t_test *test)
 
 	bzero(buf, 10);
 	strcpy(buf, "un deux 9");
-	mt_assert(strcmp(strnstr(buf, "deux", 10),  ft_strnstr(buf, "deux", 10)) == 0);
-	mt_assert(strnstr(buf, "9", 3) == ft_strnstr(buf, "9", 3));
+	mt_assert(strcmp(ft_strnstr(buf, "deux", 10), "deux 9") == 0);
+	mt_assert(ft_strnstr(buf, "9", 3) == NULL);
 	mt_assert(strcmp(ft_strnstr(buf, "", 6), buf) == 0);
-	mt_assert(ft_strnstr(buf, "deux", 5) == strnstr(buf, "deux", 5));
-	mt_assert(strcmp(ft_strnstr(buf, "9", 10), strnstr(buf, "9", 10)) == 0);
-	mt_assert(ft_strnstr(buf, "9", 8) == strnstr(buf, "9", 8));
+	mt_assert(ft_strnstr(buf, "deux", 5) == NULL);
+	mt_assert(strcmp(ft_strnstr(buf, "9", 10), "9") == 0);
+	mt_assert(ft_strnstr(buf, "9", 8) == NULL);
 }
 
 static void unittest2(t_test *test)
@@ -22,10 +22,10 @@ static void unittest2(t_test *test)
 	bzero(buf, 10);
 	strcpy(buf, "un deux 9");
 	buf[9] = '6';
-	mt_assert(strcmp(strnstr(buf, "deux", 10), ft_strnstr(buf, "deux", 10)) == 0);
-	mt_assert(strnstr(buf, "cinq", 10) == ft_strnstr(buf, "cinq", 10));
-	mt_assert(strnstr(buf, "9682", 10) == ft_strnstr(buf, "9682", 10));
-	mt_assert(strcmp(strnstr(buf, "6", 10), ft_strnstr(buf, "6", 10)) == 0);
+	mt_assert(strcmp(ft_strnstr(buf, "deux", 10), "deux 96") == 0);
+	mt_assert(ft_strnstr(buf, "cinq", 10) == NULL);
+	mt_assert(ft_strnstr(buf, "9682", 10) == NULL);
+	mt_assert(strcmp(ft_strnstr(buf, "6", 10), "6") == 0);
 }
 
 static void unittest3(t_test *test)
@@ -36,14 +36,14 @@ static void unittest3(t_test *test)
 	strcpy(buf, "un deux 9");
 	buf[9] = '6';
 	buf[1] = 0;
-	mt_assert(strnstr(buf, "deux", 10) == ft_strnstr(buf, "deux", 10));
+	mt_assert(ft_strnstr(buf, "deux", 10) == 0);
 }
 
 static void unittest4(t_test *test)
 {
-	char	buf2[] = "ozarabozaraboze";
+	char	buf2[] = "ozarabozaraboze123";
 
-	mt_assert(strnstr(buf2, "ozaraboze", 15) == ft_strnstr(buf2, "ozaraboze", 15));
+	mt_assert(strcmp(ft_strnstr(buf2, "ozaraboze", 15), "ozaraboze123") == 0);
 }
 
 void	suite_00_part1_ft_strnstr(t_suite *suite)

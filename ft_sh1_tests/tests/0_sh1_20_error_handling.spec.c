416f0b211a0e966928ebdaf815046ceee9a61e0d
yyang42 <yan.yaang@gmail.com>
2015-01-26 18:15:11 +0100

    sh1: test empty lines

--- ft_sh1_tests/tests/0_sh1_20_error_handling.spec.c
+++ ft_sh1_tests/tests/0_sh1_20_error_handling.spec.c
@@ -25,9 +25,27 @@ static void handle_tabs_spaces(t_test *test)
 		"grep 'moulitest'");
 }
 
+static void empty_line_should_not_send_err(t_test *test)
+{
+	// test->debug = 1;
+	mt_assert_sh_stderr_is_empty(test,
+		"\n"
+		"exit\n");
+}
+
+static void empty_line_with_blank_chars(t_test *test)
+{
+	// test->debug = 1;
+	mt_assert_sh_stderr_is_empty(test,
+		" \t\t\t    \n"
+		"exit\n");
+}
+
 void	suite_0_sh1_20_error_handling(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, cmd_does_not_exist);
 	SUITE_ADD_TEST(suite, cmd_exist_but_not_in_path);
 	SUITE_ADD_TEST(suite, handle_tabs_spaces);
+	SUITE_ADD_TEST(suite, empty_line_should_not_send_err);
+	SUITE_ADD_TEST(suite, empty_line_with_blank_chars);
 }

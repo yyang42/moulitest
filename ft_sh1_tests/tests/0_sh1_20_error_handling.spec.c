f7644a0fde2542f1c5a3f5c2a4dc636e1211f544
yyang42 <yan.yaang@gmail.com>
2015-01-26 18:05:40 +0100

    sh1: update 20 err

--- ft_sh1_tests/tests/0_sh1_20_error_handling.spec.c
+++ ft_sh1_tests/tests/0_sh1_20_error_handling.spec.c
@@ -2,7 +2,7 @@
 
 static void cmd_does_not_exist(t_test *test)
 {
-	test->debug = 1;
+	// test->debug = 1;
 	mt_assert_sh_stderr_not_empty(test,
 		"thisCommandDoesNotExist\n"
 		"exit\n");
@@ -10,14 +10,24 @@ static void cmd_does_not_exist(t_test *test)
 
 static void cmd_exist_but_not_in_path(t_test *test)
 {
-	test->debug = 1;
+	// test->debug = 1;
 	mt_assert_sh_stderr_not_empty(test,
 		"unsetenv PATH\nls\n"
 		"exit\n");
+}
+
+static void handle_tabs_spaces(t_test *test)
+{
+	// test->debug = 1;
+	mt_assert_sh_stdout(test,
+		" \t\t\t    echo 'moulitest'  \t\t\t   \t\n"
+		"exit\n",
+		"grep 'moulitest'");
 }
 
 void	suite_0_sh1_20_error_handling(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, cmd_does_not_exist);
 	SUITE_ADD_TEST(suite, cmd_exist_but_not_in_path);
+	SUITE_ADD_TEST(suite, handle_tabs_spaces);
 }

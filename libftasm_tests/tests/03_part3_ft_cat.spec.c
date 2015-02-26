e14e31d761491fd869cc2f21a4dae5a66ac7cb7b
yyang42 <yan.yaang@gmail.com>
2015-02-26 21:51:57 +0100

    update cat

--- libftasm_tests/tests/03_part3_ft_cat.spec.c
+++ libftasm_tests/tests/03_part3_ft_cat.spec.c
@@ -3,30 +3,32 @@
 
 static void simple_test(t_test *test)
 {
-	int pids[2];
-	char buf[10];
-	int saved_stdout;
+	mt_assert(0);
+	// int pids[2];
+	// char buf[10];
+	// int saved_stdout;
 
-	saved_stdout = dup(1);
-	pipe(pids);
-	dup2(pids[1], 1);
+	// saved_stdout = dup(1);
+	// pipe(pids);
+	// dup2(pids[1], 1);
 
-	int fd;
+	// int fd;
 
-	system("echo '12345678912345678' > catout");
-	fd = open("catout", O_RDONLY);
-	if (fd == -1)
-		return ;
-	ft_cat(fd);
-	read(pids[0], buf, 17);
-	dup2(saved_stdout, 1);
+	// system("echo '12345678912345678' > catout");
+	// fd = open("catout", O_RDONLY);
+	// if (fd == -1)
+	// 	return ;
+	// ft_cat(fd);
+	// read(pids[0], buf, 17);
+	// dup2(saved_stdout, 1);
 
-	close(fd);
-	close(pids[0]);
-	close(pids[1]);
-	close(saved_stdout);
-	system("rm catout");
-	mt_assert(memcmp(buf, "12345678912345678", 17) == 0);
+	// close(fd);
+	// close(pids[0]);
+	// close(pids[1]);
+	// close(saved_stdout);
+	// system("rm catout");
+	// mt_assert(memcmp(buf, "12345678912345678", 17) == 0);
+	// ft_cat(-1);
 }
 
 void	suite_03_part3_ft_cat(t_suite *suite)

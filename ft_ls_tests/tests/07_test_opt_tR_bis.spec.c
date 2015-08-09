f5e4aca1b4cd975b8274201392b4a396c06b4138
yyang42 <yan.yaang@gmail.com>
2015-08-09 10:25:01 +0200

    update ls_fake 4

--- ft_ls_tests/tests/07_test_opt_tR_bis.spec.c
+++ ft_ls_tests/tests/07_test_opt_tR_bis.spec.c
@@ -9,7 +9,7 @@ static void simple_test_0(t_test *test)
 
 	sandbox_cmd("mkdir -p sbox/level1_{1..2}/level2_{1..2}/level3_{1..2}");
 	sandbox_cmd("mkdir -p sbox1/level1_{1..2}/level2_{1..2}/level3_{1..2}");
-	sandbox_cmd("mkdir -p sbox/lvl1_1 sbox/lvl1_1/lvl2_1");
+	sandbox_cmd("mkdir -p sbox/level1_1 sbox/level1_1/level2_1");
 	sandbox_cmd("touch -t 201212101830.55 sbox/c_lvl1");
 	sandbox_cmd("touch -t 201212101830.55 sbox/level1_1/c_lvl2");
 	sandbox_cmd("touch -t 201212101830.55 sbox/level1_1/level2_1/c_lvl3");

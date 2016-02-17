d5e764e1455df6d42b7469e6044e62852d1e2960
marc3825 <marc.tassetti@gmail.com>
2016-02-17 17:58:20 +0100

    Check if itoa's output is malloc'd

--- libft_tests/tests/01_part2_ft_itoa.spec.c
+++ libft_tests/tests/01_part2_ft_itoa.spec.c
@@ -20,10 +20,24 @@ static void test_num4(t_test *test)
 	mt_assert(strcmp(ft_itoa(-2147483648), "-2147483648") == 0);
 }
 
+static void	malloc_test(t_test *test)
+{
+	char *tmp;
+
+	tmp = ft_itoa(0);
+	free(tmp);
+	tmp = ft_itoa(-2147483648);
+	free(tmp);
+	ft_itoa(2147483647);
+	free(tmp);
+	mt_assert(1);
+}
+
 void	suite_01_part2_ft_itoa(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_num1);
 	SUITE_ADD_TEST(suite, test_num2);
 	SUITE_ADD_TEST(suite, test_num3);
 	SUITE_ADD_TEST(suite, test_num4);
+	SUITE_ADD_TEST(suite, malloc_test);
 }
